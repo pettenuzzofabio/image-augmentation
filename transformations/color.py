5c098dcd0fa31e29cc25a3b33a89e95ee1cc8d48
Fabio Pettenuzzo <pettenuzzofabio@gmail.com>
2018-10-12 13:08:31 +0200

    fix color

--- transformations/color.py
+++ transformations/color.py
@@ -15,16 +15,22 @@ def get_enhanced_image(image, enhancement = None):
 	if (enhancement == None):
 		enhancement = constants.Enhancement.get_random()
 
-	pil_im = Image.fromarray(image)
+	pil_im = Image.fromarray(image, mode = 'L')
 	if (enhancement == constants.Enhancement.brightness):
-		factor = np.random.uniform(constants.MIN_BRIGHTNESS, constants.MAX_BRIGHTNESS)
+		factor = np.random.uniform(constants.MIN_BRIGHTNESS,
+					   constants.MAX_BRIGHTNESS)
 		enhancer = ImageEnhance.Brightness(pil_im)
+
 	elif (enhancement == constants.Enhancement.contrast):
-		factor = np.random.uniform(constants.MIN_CONTRAST, constants.MAX_CONTRAST)
+		factor = np.random.uniform(constants.MIN_CONTRAST,
+					   constants.MAX_CONTRAST)
 		enhancer = ImageEnhance.Contrast(pil_im)
+
 	else:
-		factor = np.random.uniform(constants.MIN_SHARPNESS, constants.MAX_SHARPNESS)
+		factor = np.random.uniform(constants.MIN_SHARPNESS,
+					   constants.MAX_SHARPNESS)
 		enhancer = ImageEnhance.Sharpness(pil_im)
+
 	enhanced = enhancer.enhance(factor)
 	return np.array(enhanced)
 
@@ -32,7 +38,8 @@ def random_color_shade(image, channels_format = None):
 	if (channels_format == None):
 		channels_format = constants.Channels.get_random()
 
-	intensity = np.random.uniform(constants.MIN_COLOR_SHADE, constants.MAX_COLOR_SHADE)
+	intensity = np.random.uniform(constants.MIN_COLOR_SHADE,
+				      constants.MAX_COLOR_SHADE)
 	if (channels.is_monochannel(image)):
 	 	image = change_random_channel_gray(image, intensity)
 	elif (channels_format == constants.Channels.bgr):
@@ -41,6 +48,7 @@ def random_color_shade(image, channels_format = None):
 		image = change_random_hsvchannel(image, intensity)
 	else:
 		image = change_random_hlschannel(image, intensity)
+
 	return image
 
 def change_random_channel_gray(image, intensity = 0.2):
@@ -61,8 +69,10 @@ def change_random_hsvchannel(image, intensity = 0.2):
 		intensity *= 3
 	elif (channel_choice == 3):
 		intensity *= 0.7
+
 	n_shadow = random.randint(1, 4)
 	image = __change_channel(channel_choice, intensity, n_shadow, h, s, v)
+
 	return cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
 
 # makes senses only with color images
@@ -71,8 +81,10 @@ def change_random_hlschannel(image, intensity = 0.2):
 	channel_choice = random.choice((1, 3))
 	if (channel_choice == 3):
 		intensity *= 3
+
 	n_shadow = random.randint(1, 4)
 	image = __change_channel(channel_choice, intensity, n_shadow, h, l, s)
+
 	return cv2.cvtColor(image, cv2.COLOR_HLS2BGR)
 
 def __change_channel(channel_choice, intensity, n_shadow, channel_1, channel_2,  channel_3):
@@ -83,4 +95,5 @@ def __change_channel(channel_choice, intensity, n_shadow, channel_1, channel_2, 
 		channel_2 = shadow.add_n_shadows(channel_2, n_shadow, intensity, blur_scale)
 	else:
 		channel_3 = shadow.add_n_shadows(channel_3, n_shadow, intensity, blur_scale)
+
 	return cv2.merge((channel_1, channel_2, channel_3))
