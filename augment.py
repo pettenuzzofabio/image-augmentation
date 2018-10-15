20fade010851abde0d081e349445c6f063d5be41
Fabio Pettenuzzo <pettenuzzofabio@gmail.com>
2018-10-15 08:57:01 +0200

    augment

--- augment.py
+++ augment.py
@@ -6,20 +6,20 @@ import math
 import os
 import random
 
-import transformations.colors as colors
+import constants as const
+import cv2
+import numpy as np
+import transformations.color as color
 import transformations.distort as distort
 import transformations.noise as noise
 import transformations.rotation as rotation
 import transformations.shadow as shadow
-import constants
-import cv2
-import numpy as np
 from skimage import img_as_ubyte
 from skimage import transform as tf
 
-functions_list = []
+function_list = []
 
-def get_n_augmented_images(image, n_output_list = constants.N_FILES_OUTPUT):
+def get_n_augmented_images(image, n_output_list = const.N_FILES_OUTPUT):
 	'''
 	Applies the transformations to the input image and returns a list of transformed images
 	:param image: image to be augmented
@@ -29,18 +29,19 @@ def get_n_augmented_images(image, n_output_list = constants.N_FILES_OUTPUT):
 	images_list = []
 	for _ in range(n_output_list):
 		images_list.append(get_augmented_image(image))
+
 	return images_list
 
 def get_augmented_image(image):
-	if not functions_list :
-		__init_functions_list()
+	if not function_list :
+		__init_function_list()
 
 	# TODO: add multithreading
-	tmp_functions_list = functions_list[:]
-	for _ in range(constants.N_TRANSFORMATIONS):
-		function = random.choice(tmp_functions_list)
+	tmp_function_list = function_list[ : ]
+	for _ in range(const.N_TRANSFORMATIONS):
+		function = random.choice(tmp_function_list)
 		image = function(image)
-		tmp_functions_list.remove(function)
+		tmp_function_list.remove(function)
 
 	return image
 
@@ -59,22 +60,24 @@ def write_images(full_name, images_list, output_path):
 				cv2.imwrite(output_name, image)
 			except:
 				raise
+
 		i += 1
 
-def __init_functions_list():
-	__append_function_n_times(noise.add_random_saltpepper_noise,	constants.MaxTransformations.SALT_PEPPER_NOISE)
-	__append_function_n_times(noise.get_random_speckle_noise,	constants.MaxTransformations.SPECKLE_NOISE)
-	__append_function_n_times(noise.add_random_gauss_noise, 	constants.MaxTransformations.GAUSS_NOISE)
-	__append_function_n_times(noise.add_random_blur, 		constants.MaxTransformations.BLUR)
-	__append_function_n_times(shadow.add_n_random_shadows, 		constants.MaxTransformations.SHADOW)
-	__append_function_n_times(colors.get_enhanced_image, 		constants.MaxTransformations.ENHANCEMENTS)
-	__append_function_n_times(colors.random_color_shade, 		constants.MaxTransformations.SHADE_COLORS)
-	__append_function_n_times(distort.get_random_shear, 		constants.MaxTransformations.SHEAR)
-	__append_function_n_times(distort.get_random_skew, 		constants.MaxTransformations.SKEW)
-	__append_function_n_times(distort.get_random_warp, 		constants.MaxTransformations.WARP)
-	__append_function_n_times(rotation.get_random_rotation,		constants.MaxTransformations.ROTATION)
-	constants.N_TRANSFORMATIONS = min(len(functions_list), constants.N_TRANSFORMATIONS)
+def __init_function_list():
+	t = const.MaxTransformation
+	__append_function_n_times(noise.add_random_saltpepper_noise,	t.SALT_PEPPER_NOISE)
+	__append_function_n_times(noise.get_random_speckle_noise,	t.SPECKLE_NOISE)
+	__append_function_n_times(noise.add_random_gauss_noise, 	t.GAUSS_NOISE)
+	__append_function_n_times(noise.add_random_blur, 		t.BLUR)
+	__append_function_n_times(shadow.add_n_random_shadows, 		t.SHADOW)
+	__append_function_n_times(color.get_enhanced_image, 		t.ENHANCEMENTS)
+	__append_function_n_times(color.random_color_shade, 		t.SHADE_COLOR)
+	__append_function_n_times(distort.get_random_shear, 		t.SHEAR)
+	__append_function_n_times(distort.get_random_skew, 		t.SKEW)
+	__append_function_n_times(distort.get_random_warp, 		t.WARP)
+	__append_function_n_times(rotation.get_random_rotation,		t.ROTATION)
+	const.N_TRANSFORMATIONS = min(len(function_list),		const.N_TRANSFORMATIONS)
 
 def __append_function_n_times(function, n):
 	for _ in range(n):
-		functions_list.append(function)
+		function_list.append(function)
