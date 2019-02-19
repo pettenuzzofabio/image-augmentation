d85b2455923ff9c42d9c131cf56eba0206524513
pettenuzzofabio <pettenuzzofabio@gmail.com>
2019-02-19 22:15:40 +0100

    main

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from __future__ import division
 
 import argparse
 import os
-import sys
 import time
-from os import listdir
-from os.path import isfile, join
-
-import cv2
 
 import augment
 import constants
-
 import utils.file as file
 import utils.validation as validation
+
 
 def main(args):
     image_file_name_list = file.get_files_path(args.input)
@@ -28,12 +23,11 @@ def main(args):
         labels = file.read_labels(path, image_name)
 
         start = time.time()
-        images_and_labels = augment.get_n_augmented_images_labels(image, labels, n_transformations)
+        images_and_labels_generator = augment.generate_n_augmented_images_labels(image, labels, n_transformations)
+        file.write_output_files(image_name, images_and_labels_generator, output_path)
         end = time.time()
         print("Image " + image_name + " augmented " + str(n_transformations) +
               " times in " + str("%.3f" % (end - start)) + " seconds")
-
-        file.write_output_files(image_name, images_and_labels, output_path)
 
 
 def __parse_arguments():
