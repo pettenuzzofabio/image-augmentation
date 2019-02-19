a19f2831f3c205ffd66c54188a5a282e315c6e60
pettenuzzofabio <pettenuzzofabio@gmail.com>
2019-02-19 22:16:45 +0100

    constants

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ from enum import Enum
 ### CONSTANTS MEANT TO BE EDITED ###
 
 # default number of files given as output for each input image
-N_FILES_OUTPUT = 20
+N_FILES_OUTPUT      = 200
 
 # allowed input images extensions
 IMAGE_EXTENSIONS = [".jpg",
@@ -28,20 +28,19 @@ IMAGE_EXTENSIONS = [".jpg",
                     ]
 
 # default input files path
-FILES_INPUT_PATH = "./input/"
+FILES_INPUT_PATH        = "./input/"
 
 # default output files path
-FILES_OUTPUT_PATH = "./output/"
+FILES_OUTPUT_PATH       = "./output/"
 
 '''
 max number of transformations
 that are randomly applied for each output image.
 If N_TRANSFORMATIONS > total number of transformations
 specified in the MaxTransformation field
-N_TRANSFORMATIONS will be reset as:
-min( sum_MaxTransformation_fields, N_TRANSFORMATIONS) )
+N_TRANSFORMATIONS will be reset as sum_MaxTransformation_fields
 '''
-N_TRANSFORMATIONS = 2
+N_TRANSFORMATIONS       = 1
 
 '''
 MaxTransformation contains, for each transformation,
@@ -77,69 +76,71 @@ MIN_GAUSS_NOISE         = 1
 MAX_GAUSS_NOISE         = 100
 
 # MIN/MAX SALT AND PEPPER NOISE
-MIN_SALT_PEPPER_NOISE = 0.0001
-MAX_SALTPEPPER_NOISE = 0.001
+MIN_SALT_PEPPER_NOISE   = 0.0001
+MAX_SALTPEPPER_NOISE    = 0.001
 
 # MIN/MAX SPECKLE
-MIN_SPECKLE_NOISE = 0.01
-MAX_SPECKLE_NOISE = 0.3
+MIN_SPECKLE_NOISE       = 0.01
+MAX_SPECKLE_NOISE       = 0.3
 
 # MIN/MAX SHADOW
-MIN_SHADOW = 0.3
-MAX_SHADOW = 0.7
+MIN_SHADOW              = 0.3
+MAX_SHADOW              = 0.7
 
 # MIN/MAX IMAGE BRIGHTNESS
-MIN_BRIGHTNESS = 0.6
-MAX_BRIGHTNESS = 1.4
+MIN_BRIGHTNESS          = 0.6
+MAX_BRIGHTNESS          = 1.4
 
 # MIN/MAX IMAGE CONTRAST
-MIN_CONTRAST = 0.5
-MAX_CONTRAST = 1.7
+MIN_CONTRAST            = 0.5
+MAX_CONTRAST            = 1.7
 
 # MIN/MAX IMAGE SHARPNESS
-MIN_SHARPNESS = 0.1
-MAX_SHARPNESS = 5.0
+MIN_SHARPNESS           = 0.1
+MAX_SHARPNESS           = 5.0
 
 # MIN/MAX COLOR SHADING
-MIN_COLOR_SHADE = 0.06
-MAX_COLOR_SHADE = 0.35
+MIN_COLOR_SHADE         = 0.06
+MAX_COLOR_SHADE         = 0.35
 
 # MAX SHEAR DISTORTION
-MAX_SHEAR = 0.05
+MAX_SHEAR               = 0.05
 
 # MAX SKEW DISTORTION
-MAX_SKEW = 0.05
+MAX_SKEW                = 0.05
 
 # MIN/MAX WARP DISTORTION
-MIN_WARP = 14
-MAX_WARP = 51
+MIN_WARP                = 14
+MAX_WARP                = 51
 
 # MIN/MAX ROTATION ANGLE
-MAX_ANGLE = 0.02
+MAX_ANGLE               = 0.02
 
-# By default s&p and speckle noise
+# By default salt&pepper and speckle noise
 # is followed by blurring
-ADD_BLUR_AFTER_SPECKLE_NOISE = False
-ADD_BLUR_AFTER_SP_NOISE = False
+ADD_BLUR_AFTER_SPECKLE_NOISE    = False
+ADD_BLUR_AFTER_SP_NOISE         = False
 
-READ_IMAGE_AS_GRAYSCALE = False
+READ_IMAGE_AS_GRAYSCALE         = False
 
 ### PAY ATTENTION BEFORE EDITING THE FOLLOWING CONSTANTS ###
 
-BLACK = 0
-LIGHT_BLACK = 50
-DARK_GRAY = 100
-GRAY = 150
-LIGHT_GRAY = 200
-DARK_WHITE = 250
-WHITE = 255
+BLACK           = 0
+LIGHT_BLACK     = 50
+DARK_GRAY       = 100
+GRAY            = 150
+LIGHT_GRAY      = 200
+DARK_WHITE      = 250
+WHITE           = 255
 
-SEED = 1  # random.random()
+SEED = random.random()
+# Max integer value for Python 2. Integers in Python 3 are unbounded
+MAX_RANDOM = 2147483648
 
 class Enhancement(Enum):
-    brightness = 0
-    contrast = 1
-    sharpness = 2
+    brightness  = 0
+    contrast    = 1
+    sharpness   = 2
 
     @staticmethod
     def get_random():
@@ -147,9 +148,9 @@ class Enhancement(Enum):
 
 
 class Channels(Enum):
-    bgr = 0
-    hsv = 1
-    hls = 2
+    bgr         = 0
+    hsv         = 1
+    hls         = 2
 
     @staticmethod
     def get_random():
